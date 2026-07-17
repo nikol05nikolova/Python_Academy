b681f8cfd195e249abbc0e69a5036dcc740c9665
Nikol Nikolova <nikol05nikolova@gmail.com>
2026-07-18 00:47:11 +0300

    create pygame game loop

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Python Academy")
+        self.screen = pygame.display.set_mode((800, 600))
+        self.clock = pygame.time.Clock()
+        
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.screen.fill((30, 30, 30))
+            pygame.display.flip()
+            self.clock.tick(60)
+
+def main():
+    game = Game()
+    game.run()
+
+if __name__ == "__main__":
+    main()
