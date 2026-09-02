d69a04f2ed0ba760f8da99dc0e3cf1cb3010d86a
Nikol Nikolova <nikol05nikolova@gmail.com>
2026-09-03 00:06:37 +0300

    add snickers counter

--- game.py
+++ game.py
@@ -12,6 +12,7 @@ class Game:
         pygame.display.set_caption(WINDOW_TITLE)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font(None, 32)
         self.player_1 = Player(
             PLAYER_1_START_X,
             PLAYER_1_START_Y,
@@ -46,6 +47,19 @@ class Game:
             Snickers(230, 305, SNICKERS_WIDTH, SNICKERS_HEIGHT, SNICKERS_BLUE_COLOR, 1),
             Snickers(580, 215, SNICKERS_WIDTH, SNICKERS_HEIGHT, SNICKERS_YELLOW_COLOR, 2),
         ]
+        self.collected_snickers = 0
+
+    def draw_ui(self):
+        collected = self.collected_snickers
+        total = len(self.snickers)
+
+        text = self.font.render(
+            f"Snickers: {collected} / {total}",
+            True,
+            (255, 255, 255)
+        )
+
+        self.screen.blit(text, (20, 20))
 
     def run(self):
         while True:
@@ -62,15 +76,27 @@ class Game:
             self.player_1.update(self.platforms)
             self.player_2.update(self.platforms)
             for snickers in self.snickers:
-                if (self.player_1.rect.colliderect(snickers.rect) and snickers.player_number == 1):
+                if (
+                    not snickers.collected
+                    and self.player_1.rect.colliderect(snickers.rect)
+                    and snickers.player_number == 1
+                ):
                     snickers.collect()
-                if (self.player_2.rect.colliderect(snickers.rect) and snickers.player_number == 2):
+                    self.collected_snickers += 1
+
+                if (
+                    not snickers.collected
+                    and self.player_2.rect.colliderect(snickers.rect)
+                    and snickers.player_number == 2
+                ):
                     snickers.collect()
+                    self.collected_snickers += 1
             for platform in self.platforms:
                 platform.draw(self.screen)
             for snickers in self.snickers:
                 snickers.draw(self.screen)
             self.player_1.draw(self.screen)
             self.player_2.draw(self.screen)
+            self.draw_ui()
             pygame.display.update()
             self.clock.tick(60)
