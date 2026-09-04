ae957d112fab94f3c28d036d93d1adcba95ba975
Nikol Nikolova <nikol05nikolova@gmail.com>
2026-09-04 23:22:41 +0300

    fix main

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from menu import Menu
+from level_select import LevelSelect
+from level_loader import list_levels
 from game import Game
 
 
 def main():
-    game = Game()
-    game.run()
+    menu = Menu()
+    level_select = LevelSelect(list_levels())
+    while True:
+        menu.run()
+        level_path = level_select.run()
+        game = Game(level_path)
+        game.run()
 
 
 if __name__ == "__main__":
